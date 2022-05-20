9ea4b1261b34c5b8117e03e34e9be6adf1039b34
HarishB167 <harishbb168@gmail.com>
2022-05-20 23:50:20 +0530

    Methods vid completed

--- IntermediateCourse/Program.cs
+++ IntermediateCourse/Program.cs
@@ -10,9 +10,42 @@ namespace IntermediateCourse
     {
         static void Main(string[] args)
         {
-            var customer = new Customer(1, "John");
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(100, 200);
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(null);
+
         }
     }
 }
